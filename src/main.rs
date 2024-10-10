296200f35c112b0c931210b613e4b8271aeef02e
Andrew Binstock <920630+platypusguy@users.noreply.github.com>
2024-10-10 16:55:41 -0700

    Slowly adding basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::path::Path;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    // Collect the command line arguments into a vector
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the program's name, which can generally be ignored
+    // let program_name = &args[0];
+
+    // Check if a file name was specified
+    if args.len() == 1 { // if none 
+        println!("Usage: javap path_to_class_file");
+        exit(1)
+    }
+
+    let file_path = &args[1];
+
+    // Create a Path object
+    let path = Path::new(file_path);
+
+    // Check if the path exists and is a file
+    if path.exists() && path.is_file() {
+        println!("The file {} exists!", file_path);
+        if path.is_file() {
+            println!("and {} is a file.", file_path);
+        } else {
+            println!("but it's not a file.");
+            exit(0)
+        }
+    } else {
+        println!("The file {} does not exist.", file_path);
+        exit(0)
+    }
 }
+   
